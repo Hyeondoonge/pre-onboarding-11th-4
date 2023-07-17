5b2ac969403e7f5ff5565a32a77ea664174d7f83
Hyeondoonge <migu554@naver.com>
2023-07-18 01:23:20 +0900

    feat: 캐싱 데이터 타입검사 및 만료기한 검사 기능 추가

---
+++ src/utils/cache.ts
@@ -0,0 +1,19 @@
+import { TCachedResult } from 'types/common';
+
+export const isCachedResult = (data: any | TCachedResult): data is TCachedResult => {
+  if (!data) return false;
+  if (!('expired_time' in data)) return false;
+  if (!('result' in data)) return false;
+  if (Array.isArray(data.result)) return false;
+
+  for (const item of data) {
+    if (!('sickCd' in item)) return false;
+    if (!('sickNm' in item)) return false;
+  }
+  return true;
+};
+
+export const isValidateExpiredDate = (expired_time: number): boolean => {
+  const currentTime = Date.now();
+  return currentTime < expired_time;
+};
