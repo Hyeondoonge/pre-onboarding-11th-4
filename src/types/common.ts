ada2aa020a4c895226f4f56df8920c2475e46734
Hyeondoonge <migu554@naver.com>
2023-07-18 01:24:16 +0900

    feat: 공통으로 쓰이는 타입 분리

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,2 @@
-export type TResult = { sickCd: number; sickNm: string }[];
+export type TResult = { sickCd: number; sickNm: string };
+export type TCachedResult = { result: TResult; expired_time: number };
