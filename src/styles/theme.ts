fd53c8e4619b3aa08c7a54aefb3003bd9f3f1278
Hyeondoonge <migu554@naver.com>
2023-07-18 22:27:27 +0900

    design: text border, text background 테마 속성 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,10 +4,12 @@ export const Dark: DefaultTheme = {
   main: '#D4D4D4',
   secondary: '#B7B7B7',
   placeholder: '#A0A0A0',
-  background: '#0D161E',
+  background: '#101316',
+  textBackground: 'transparent',
   icons: '#C6C6C6',
   border: '#A1A1A1',
-  focused: '#333333'
+  focused: '#333333',
+  textBorder: '1px solid #A1A1A1'
 };
 
 export const Light: DefaultTheme = {
@@ -15,8 +17,10 @@ export const Light: DefaultTheme = {
   secondary: '#A1A1A1',
   placeholder: '#C3C3C3',
   background: '#B1DAFF',
+  textBackground: '#FAFAFA',
   icons: '#838383',
   border: '#C7C7C7',
   focused: '#F1F1F1',
-  shadow: '#ABABAB'
+  shadow: '#ABABAB',
+  textBorder: 'none'
 };
